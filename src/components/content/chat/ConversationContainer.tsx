55c08c7f26e47353777c81faaf047ad6b062d4cd
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2024-10-22 22:50:39 +0200

    feat: add autoscrolling to conversation

--- src/components/content/chat/ConversationContainer.tsx
+++ src/components/content/chat/ConversationContainer.tsx
@@ -1,24 +1,35 @@
 import * as ScrollArea from '@radix-ui/react-scroll-area'
 import { MessageRole, type Conversation } from '../../../types/types'
 import { MessageContainer } from './MessageContainer'
+import { useEffect, useRef } from 'react'
 
 interface Props {
   conversation: Conversation
 }
 
-export const ConversationContainer = ({ conversation }: Props) => (
-  <ScrollArea.Root>
-    <ScrollArea.Viewport>
-      <div className='flex flex-col gap-2 h-[300px]'>
-        {conversation.messages.map(({ role, id, text }) => {
-          const isUser = role === MessageRole.USER
-          return <MessageContainer text={text} isUser={isUser} key={id} />
-        })}
-      </div>
-    </ScrollArea.Viewport>
-    <ScrollArea.Scrollbar orientation='vertical'>
-      <ScrollArea.Thumb />
-    </ScrollArea.Scrollbar>
-    <ScrollArea.Corner />
-  </ScrollArea.Root>
-)
+export const ConversationContainer = ({ conversation }: Props) => {
+  const scrollableAreaRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (scrollableAreaRef.current) {
+      scrollableAreaRef.current.scrollTop = scrollableAreaRef.current.scrollHeight
+    }
+  }, [conversation])
+
+  return (
+    <ScrollArea.Root>
+      <ScrollArea.Viewport ref={scrollableAreaRef}>
+        <div className='flex flex-col gap-2 h-[300px]'>
+          {conversation.messages.map(({ role, id, text }) => {
+            const isUser = role === MessageRole.USER
+            return <MessageContainer text={text} isUser={isUser} key={id} />
+          })}
+        </div>
+      </ScrollArea.Viewport>
+      <ScrollArea.Scrollbar orientation='vertical'>
+        <ScrollArea.Thumb />
+      </ScrollArea.Scrollbar>
+      <ScrollArea.Corner />
+    </ScrollArea.Root>
+  )
+}
